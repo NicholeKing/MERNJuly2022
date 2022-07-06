850d2fd63b387ea5381106c23779c594de65636e
NicholeKing <nichole.king94@hotmail.com>
2022-07-06 15:45:04 -0500

    first react project

---
+++ Day02/first-react/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <table>
+        <tr>
+          <th>fdksjfd</th>
+          <th>fhkdjs</th>
+        </tr>
+        <tr>
+          <td>fhdskj</td>
+          <td>fhdskj</td>
+        </tr>
+        <tr>
+          <td>fhdskj</td>
+          <td>fhdskj</td>
+        </tr>
+      </table>
+      <h4>This is a new element below the table</h4>
+      <label htmlFor="Title">Title</label>
+      <input type="text" name="Title" />
+    </div>
+  );
+}
+
+export default App;
